24f8f61dcb8d58e33594d27503be96aa9a000d55
James Patrick <nebulasoftware1@gmail.com>
2026-09-07 10:15:44 +1000

    feat: refactored decode_string and decode_integer methods to track postitions

--- deserialize.py
+++ deserialize.py
@@ -4,50 +4,59 @@ class Deserializer:
         self.position = 1
 
     def decode_string(self):
-        str_result = []
-        for i in range(self.position, len(self.value)):
-            if self.value[i] != '\r' and self.value[i] != '\n':
-                str_result.append(self.value[i])
+        end = self.value.find('\r\n', self.position)
 
-        return "".join(str_result)
+        if end == -1:
+            end = len(self.value)
+
+        current_line = self.value[self.position:end]
+
+        self.position = end + 2
+
+        return current_line
 
     def decode_integer(self):
-        int_result = []
-        for i in range(self.position, len(self.value)):
-            if self.value[i] != '\r' and self.value[i] != '\n':
-                int_result.append(self.value[i])
+        end = self.value.find('\r\n', self.position)
 
-        ext_result = "".join(int_result)
-        return int(ext_result)
+        if end == -1:
+            end = len(self.value)
+
+        current_value = self.value[self.position:end]
+
+        self.position = end + 2
+
+        return int(current_value)
 
     def decode_bulk_string(self):
-        str_count = ""
-        bulk_str_res = []
-
-        for i in range(self.position, len(self.value)):
-            if self.value[i].isdigit():
-                str_count += self.value[i]
-                if str_count == "-1":
-                    return None
-                elif str_count == '0':
-                    return ""
-            
-            if self.value[i] != "\r" and self.value[i] != "\n" and self.value[i] != str_count:
-                bulk_str_res.append(self.value[i])
-        
-        return "".join(bulk_str_res) # up to here, test if this is correct now
+        pass
 
     def decode_array(self):
-        pass
+        result = ""
+        clean_value = self.value.replace("/r", "").replace("\n", "")
+        for i in range(self.position, len(clean_value)):
+            if clean_value[i] == '$':
+                for item in clean_value:
+                    content = Deserializer(item).decoder_dispatch()
+                    result += content
+        
+        return result
 
     def decoder_dispatch(self):
         if self.value[0] == '+':
-            return self.decode_string
+            self.position += 1
+            return self.decode_string()
         elif self.value[0] == '-':
+            self.position += 1
             return self.decode_string()    
         elif self.value[0] == ':':
+            self.position += 1
             return self.decode_integer()    
         elif self.value[0] == '$':
+            self.position += 1
             return self.decode_bulk_string()    
         elif self.value[0] == '*':
+            self.position += 1
             return self.decode_array()
+
+#d = Deserializer("*2\r\n$3\r\nfoo\r\n$3\r\nbar\r\n")
+#d.decoder_dispatch()
