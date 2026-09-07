35dd166da83030f9b73bbff036afe8076db75a60
James Patrick <nebulasoftware1@gmail.com>
2026-09-08 09:30:41 +1000

    feat: integrated Serializer and Deserializer into server loop, handle PING and ECHO commands

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 import socket
+from serialize import Serializer
+from deserialize import Deserializer
 
 HOST = "127.0.0.1"
 PORT = 6379
@@ -9,16 +11,32 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
     server_socket.listen()
     print(f"Server is listening on port {HOST}:{PORT}...")
 
-    conn, addr = server_socket.accept()
+    while True:
+        conn, addr = server_socket.accept()
+        with conn:
+            while True:
+                print(f"Connected successfully by client at: {addr}")
 
-    with conn:
-        print(f"Connected successfully by client at: {addr}")
+                # read data from connection
+                data = conn.recv(1024) 
 
-        # read data from connection
-        data = conn.recv(1024) 
+                if not data:
+                    break
 
-        if not data:
-            print("Client disconnected without sending data")
-        else:
-            print(f"Received data from client: {data.decode('utf-8')}")
+                data_str = data.decode('utf-8')
+                parsed = Deserializer(data_str).decoder_dispatch()
+                
+                command = parsed[0].lower()
+
+                if command == "ping":
+                    response = Serializer("PONG").encode_str()
+
+                elif command == "echo":
+                    message = parsed[1]
+                    response = Serializer(message).encode_bulk_string()
+                
+                else:
+                    response = Serializer("ERR unknown command").encode_error()
+
+                conn.send(response.encode('utf-8'))
 
